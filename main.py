66bfbad39601a3bf6da62823e2054df44e937f58
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2025-09-26 22:52:56 +0300

    added system prompt

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 def main():
     verbose = "--verbose" in sys.argv
     args = []
@@ -32,7 +32,10 @@ def main():
     ]
 
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages)
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        config=types.GenerateContentConfigDict(system_instruction=system_prompt),
+        )
     
     print(response.text)
 
