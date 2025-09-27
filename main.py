b4e540d39375cb9f5bcf0eee0dadc0c105edb9c9
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2025-09-27 17:00:14 +0300

    added function calling capability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from dotenv import load_dotenv
+from functions.get_files_info import schema_get_files_info 
 from google import genai
 from google.genai import types
 
@@ -8,7 +9,22 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 def main():
     verbose = "--verbose" in sys.argv
     args = []
@@ -34,10 +50,17 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
-        config=types.GenerateContentConfigDict(system_instruction=system_prompt),
+        config=types.GenerateContentConfigDict(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+            )
         )
     
-    print(response.text)
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
