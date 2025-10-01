1b101284a67221e78b1a4d10f9da5e8ce3f16143
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2025-10-01 20:56:01 +0100

    added agent functionality

--- functions/call_function.py
+++ functions/call_function.py
@@ -18,24 +18,8 @@ def call_function(function_call_part, verbose=False):
         print(f" - Calling function: {function_call_part.name}")
     
     if function_call_part.name not in function_dict:
-        return types.Content(
-            role="tool",
-            parts=[
-                types.Part.from_function_response(
-                    name=function_call_part.name,
-                    response={"error": f"Unknown function: {function_call_part.name}"},
-                )
-            ],
-        )
+        return {"Error": f"Unknown function: {function_call_part.name}"}
     else:
         args = dict(function_call_part.args or {})
         args["working_directory"] = "./calculator"
-        return types.Content(
-            role="tool",
-            parts=[
-                types.Part.from_function_response(
-                    name=function_call_part.name,
-                    response={"result": function_dict[function_call_part.name](**args)},
-                )
-            ],
-        )
+        return {"result": function_dict[function_call_part.name](**args)}
